373bc6a6050cd79eb6775558096d2328654b4e34
esports tunisia <esports.tunisia@gmail.com>
2020-12-13 14:39:27 +0000

    port changed

--- src/main.ts
+++ src/main.ts
@@ -18,6 +18,6 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, options);
   SwaggerModule.setup('', app, document);
 
-  await app.listen(3000);
+  await app.listen(5000);
 }
 bootstrap();
